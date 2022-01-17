2fb2e80d87f7291b6e31fe517dcd3fc7a94b170c
Olivier Winsemius <o.winsemius@gmail.com>
2022-01-17 19:37:55 +0100

    test: expanding tests

--- src/tests/field-validator.test.ts
+++ src/tests/field-validator.test.ts
@@ -19,24 +19,42 @@ describe("FieldValidator", () => {
 
   const tests: Test[] = [
     [5, (v) => v.number(), []],
-    [5, (v) => v.maxNumber(10), []],
     ["test", (v) => v.number(), ["invalid_type_number"]],
+    [null, (v) => v.number(), ["invalid_type_number"]],
+    [undefined, (v) => v.number(), ["invalid_type_number"]],
 
     [null, (v) => v.number().nullable(), []],
-    [undefined, (v) => v.string().maybe(), []],
+    [5, (v) => v.number().nullable(), []],
+    ["test", (v) => v.number().nullable(), ["invalid_type_number"]],
 
+    [undefined, (v) => v.number().maybe(), []],
+    [5, (v) => v.number().maybe(), []],
+    ["test", (v) => v.number().maybe(), ["invalid_type_number"]],
+
+    ["test", (v) => v.string(), []],
     [5, (v) => v.string(), ["invalid_type_string"]],
+
+    [date2020, (v) => v.date(), []],
     [5, (v) => v.date(), ["invalid_type_date"]],
+
+    [true, (v) => v.boolean(), []],
     [5, (v) => v.boolean(), ["invalid_type_boolean"]],
+
+    [true, (v) => v.custom(() => undefined), []],
     [5, (v) => v.custom(() => "custom_error"), ["custom_error"]],
-    [5, (v) => v.minNumber(10), ["invalid_value_min_number"]],
+
     [5, (v) => v.minNumber(0), []],
-    [5, (v) => v.maxNumber(0), ["invalid_value_max_number"]],
+    [5, (v) => v.minNumber(10), ["invalid_value_min_number"]],
+
     [5, (v) => v.maxNumber(10), []],
-    [date2020, (v) => v.minDate(date2040), ["invalid_value_min_date"]],
+    [5, (v) => v.maxNumber(0), ["invalid_value_max_number"]],
+
     [date2020, (v) => v.minDate(date2000), []],
-    [date2020, (v) => v.maxDate(date2000), ["invalid_value_max_date"]],
+    [date2020, (v) => v.minDate(date2040), ["invalid_value_min_date"]],
+
     [date2020, (v) => v.maxDate(date2040), []],
+    [date2020, (v) => v.maxDate(date2000), ["invalid_value_max_date"]],
+
     ["test", (v) => v.oneOf(v.string, v.number), []],
     [
       false,
