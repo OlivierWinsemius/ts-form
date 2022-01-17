a3c0b81a589bf47f9fff16d5b3b5f26a463e30e8
Olivier Winsemius <o.winsemius@gmail.com>
2022-01-17 19:39:47 +0100

    chore: remove eslint-ignore

--- src/utils/object-from-keys.ts
+++ src/utils/object-from-keys.ts
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-export const objectFromKeys = <O extends Record<string, any>, V>(
+export const objectFromKeys = <O extends Record<string, unknown>, V>(
   originalObject: O,
   getValue: (key: keyof O) => V
 ): Record<keyof O, V> => {
   const keys = Object.keys(originalObject);
+
   const entries = new Map(
     keys.map<[keyof O, V]>((key) => [key, getValue(key)])
   );
+
   return Object.fromEntries(entries) as Record<keyof O, V>;
 };
